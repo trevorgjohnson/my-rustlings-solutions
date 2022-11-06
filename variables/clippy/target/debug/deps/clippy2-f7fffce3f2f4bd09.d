3dd3a5fa9e8f7f213b2f61153bbd6ce39df8f0e7
Trevor Johnson <27569194+trevorgjohnson@users.noreply.github.com>
2022-11-06 10:56:57 -0600

    Initial Commit

---
+++ variables/clippy/target/debug/deps/clippy2-f7fffce3f2f4bd09.d
@@ -0,0 +1,7 @@
+/Users/trevorjohnson/rustlings/exercises/clippy/target/debug/deps/clippy2-f7fffce3f2f4bd09.rmeta: clippy2.rs
+
+/Users/trevorjohnson/rustlings/exercises/clippy/target/debug/deps/clippy2-f7fffce3f2f4bd09.d: clippy2.rs
+
+clippy2.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
